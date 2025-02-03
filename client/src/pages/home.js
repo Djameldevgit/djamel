e918a6dcab5fbc0bd1708ff9cd8187f897806ea9
Djameldevgit <developpementwebdjamel@gmail.com>
2025-02-03 23:55:13 +0000

    jjjjj

---
+++ client/src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+             <h2>Línea con `content-box`:</h2>
+    <div class="linea-content-box"></div>
+
+    <h2>Línea con `border-box`:</h2>
+    <div class="linea-border-box"></div>
+        </div>
+
+    )
+}
+
+export default Home
