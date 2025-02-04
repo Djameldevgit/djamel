ae973b251b23044812b8c6815cef601a0c8b90bb
Djameldevgit <developpementwebdjamel@gmail.com>
2025-02-04 00:51:21 +0000

    hhhhh

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import Status from '../components/home/Status'
+import Posts from '../components/home/Posts'
+ 
+
+import { useSelector } from 'react-redux'
+import LoadIcon from '../images/loading.gif'
+
+
+let scroll = 0;
 
 const Home = () => {
+    const { homePosts } = useSelector(state => state)
+
+    window.addEventListener('scroll', () => {
+        if(window.location.pathname === '/'){
+            scroll = window.pageYOffset
+            return scroll;
+        }
+    })
+
+    useEffect(() => {
+        setTimeout(() => {
+            window.scrollTo({top: scroll, behavior: 'smooth'})
+        }, 100)
+    },[])
+
     return (
-        <div>
-             <h2>Línea con `content-box`:</h2>
-    <div class="linea-content-box"></div>
+        <div className="home mx-0">
+            
+                <Status />
 
-    <h2>Línea con `border-box`:</h2>
-    <div class="linea-border-box"></div>
+                {
+                    homePosts.loading 
+                    ? <img src={LoadIcon} alt="loading" className="d-block mx-auto" />
+                    : (homePosts.result === 0 && homePosts.posts.length === 0)
+                        ? <h2 className="text-center">No Post</h2>
+                        : <Posts />
+                }
+                
+           
+            
+           
         </div>
-
     )
 }
 
